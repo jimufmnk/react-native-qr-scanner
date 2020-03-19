8500ed092103ff1689c9a6f9dcad249102970e95
Angel <angel@Angels-MacBook-Pro.local>
2020-03-19 15:36:37 -0600

    Added android files

---
+++ android/src/main/java/com/qrcode/QRScanReader.java
@@ -0,0 +1,231 @@
+package com.lewin.qrcode;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.ChecksumException;
+import com.google.zxing.DecodeHintType;
+import com.google.zxing.FormatException;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.PlanarYUVLuminanceSource;
+import com.google.zxing.RGBLuminanceSource;
+import com.google.zxing.ReaderException;
+import com.google.zxing.Result;
+import com.google.zxing.common.HybridBinarizer;
+import com.google.zxing.qrcode.QRCodeReader;
+
+import java.util.Hashtable;
+
+public class QRScanReader extends ReactContextBaseJavaModule {
+
+    public QRScanReader(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+
+    @Override
+    public String getName() {
+        return "QRScanReader";
+    }
+
+    @ReactMethod
+    public void readerQR(String fileUrl, Promise promise ) {
+        Result result = scanningImage(fileUrl);
+        if(result == null){
+            promise.reject("404","Can't read file");
+        } else {
+            promise.resolve(result.getText());
+        }
+    }
+
+    /**
+     * @param path
+     * @return
+     */
+    public Result scanningImage(String path) {
+        if (path == null || path.length() == 0) {
+            return null;
+        }
+        Hashtable<DecodeHintType, String> hints = new Hashtable<>();
+        hints.put(DecodeHintType.CHARACTER_SET, "UTF8");
+
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        Bitmap scanBitmap = BitmapFactory.decodeFile(path, options);
+        options.inJustDecodeBounds = false;
+        int sampleSize = (int) (options.outHeight / (float) 200);
+        if (sampleSize <= 0)
+            sampleSize = 1;
+        options.inSampleSize = sampleSize;
+        scanBitmap = BitmapFactory.decodeFile(path, options);
+        int width=scanBitmap.getWidth();
+        int height=scanBitmap.getHeight();
+        int[] pixels=new int[width*height];
+        scanBitmap.getPixels(pixels,0,width,0,0,width,height);
+        RGBLuminanceSource source = new RGBLuminanceSource(scanBitmap.getWidth(),scanBitmap.getHeight(),pixels);
+        BinaryBitmap bitmap1 = new BinaryBitmap(new HybridBinarizer(source));
+        QRCodeReader reader = new QRCodeReader();
+        try {
+            return reader.decode(bitmap1, hints);
+        } catch (NotFoundException e) {
+            e.printStackTrace();
+        } catch (ChecksumException e) {
+            e.printStackTrace();
+        } catch (FormatException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * @param path
+     * @return
+     */
+    public static Result decodeBarcodeRGB(String path) {
+        BitmapFactory.Options opts = new BitmapFactory.Options();
+        opts.inSampleSize = 1;
+        Bitmap barcode = BitmapFactory.decodeFile(path, opts);
+        Result result = decodeBarcodeRGB(barcode);
+        barcode.recycle();
+        barcode = null;
+        return result;
+    }
+
+    /**
+     * @param barcode
+     * @return
+     */
+    public static Result decodeBarcodeRGB(Bitmap barcode) {
+        int width = barcode.getWidth();
+        int height = barcode.getHeight();
+        int[] data = new int[width * height];
+        barcode.getPixels(data, 0, width, 0, 0, width, height);
+        RGBLuminanceSource source = new RGBLuminanceSource(width, height, data);
+        BinaryBitmap bitmap1 = new BinaryBitmap(new HybridBinarizer(source));
+        QRCodeReader reader = new QRCodeReader();
+        Result result = null;
+        try {
+            result = reader.decode(bitmap1);
+        } catch (NotFoundException e) {
+            e.printStackTrace();
+        } catch (ChecksumException e) {
+            e.printStackTrace();
+        } catch (FormatException e) {
+            e.printStackTrace();
+        }
+        barcode.recycle();
+        barcode = null;
+        return result;
+    }
+
+    /**
+     * @param path
+     * @return
+     */
+    public static Result decodeBarcodeYUV(String path) {
+        if (path == null || path.length() == 0) {
+            return null;
+        }
+        BitmapFactory.Options opts = new BitmapFactory.Options();
+        opts.inSampleSize = 1;
+        Bitmap barcode = BitmapFactory.decodeFile(path, opts);
+        Result result = decodeBarcodeYUV(barcode);
+        barcode.recycle();
+        barcode = null;
+        return result;
+    }
+
+    /**
+     * @param barcode
+     * @return
+     */
+    public static Result decodeBarcodeYUV(Bitmap barcode) {
+        if (null == barcode) {
+            return null;
+        }
+        int width = barcode.getWidth();
+        int height = barcode.getHeight();
+        int[] argb = new int[width * height];
+        barcode.getPixels(argb, 0, width, 0, 0, width, height);
+        byte[] yuv = new byte[width * height * 3 / 2];
+        encodeYUV420SP(yuv, argb, width, height);
+        Result result = decodeBarcodeYUV(yuv, width, height);
+
+        barcode.recycle();
+        barcode = null;
+        return result;
+    }
+
+    /**
+     * @param yuv
+     * @param width
+     * @param height
+     * @return
+     */
+    private static Result decodeBarcodeYUV(byte[] yuv, int width, int height) {
+        long start = System.currentTimeMillis();
+        MultiFormatReader multiFormatReader = new MultiFormatReader();
+        multiFormatReader.setHints(null);
+
+        Result rawResult = null;
+        PlanarYUVLuminanceSource source = new PlanarYUVLuminanceSource(yuv, width, height, 0, 0,
+                width, height, false);
+        if (source != null) {
+            BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+            try {
+                rawResult = multiFormatReader.decodeWithState(bitmap);
+            } catch (ReaderException re) {
+                re.printStackTrace();
+            } finally {
+                multiFormatReader.reset();
+                multiFormatReader = null;
+            }
+        }
+        long end = System.currentTimeMillis();
+        return rawResult;
+    }
+
+    /**
+     * Y=0.299R+0.587G+0.114B;
+     * U=-0.147R-0.289G+0.436B;
+     * V=0.615R-0.515G-0.1B;
+     *
+     * @param yuv
+     * @param argb
+     * @param width
+     * @param height
+     */
+    private static void encodeYUV420SP(byte[] yuv, int[] argb, int width, int height) {
+        final int frameSize = width * height;
+        int Y, U, V;
+        int yIndex = 0;
+        int uvIndex = frameSize;
+        int R, G, B;
+        int rgbIndex = 0;
+        for (int j = 0; j < height; j++) {
+            for (int i = 0; i < width; i++) {
+                R = (argb[rgbIndex] & 0xff0000) >> 16;
+                G = (argb[rgbIndex] & 0xff00) >> 8;
+                B = (argb[rgbIndex] & 0xff);
+                //
+                rgbIndex++;
+                Y = ((66 * R + 129 * G + 25 * B + 128) >> 8) + 16;
+                U = ((-38 * R - 74 * G + 112 * B + 128) >> 8) + 128;
+                V = ((112 * R - 94 * G - 18 * B + 128) >> 8) + 128;
+                Y = Math.max(0, Math.min(Y, 255));
+                U = Math.max(0, Math.min(U, 255));
+                V = Math.max(0, Math.min(V, 255));
+                yuv[yIndex++] = (byte) Y;
+                if ((j % 2 == 0) && (i % 2 == 0)) {
+                    yuv[uvIndex++] = (byte) V;
+                    yuv[uvIndex++] = (byte) U;
+                }
+            }
+        }
+    }
+}
---
+++ index.js
@@ -0,0 +1,5 @@
+import { NativeModules } from 'react-native';
+
+var QRReader = NativeModules.QRScanReader;
+
+export default  QRReader
